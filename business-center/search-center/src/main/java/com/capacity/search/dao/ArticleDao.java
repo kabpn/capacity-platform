811edc7a563fc5007b6fbda39c27f841d895eab8
flame <39283511+kabpn@users.noreply.github.com>
2019-11-01 10:57:44 +0800

    Update ArticleDao.java

--- business-center/search-center/src/main/java/com/capacity/search/dao/ArticleDao.java
+++ business-center/search-center/src/main/java/com/capacity/search/dao/ArticleDao.java
@@ -5,7 +5,7 @@ import com.capacity.search.pojo.Article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
+//这是一个测试
 public interface ArticleDao extends ElasticsearchRepository<Article, String> {
 
     public Page<Article> findByTitleOrContentLike(String title, String content, Pageable pageable);
